283c7edf6114696fa7cb23d423e81a6917b6695c
kanikaaaa99999 <kooslovv@gmail.com>
2026-02-11 07:47:07 +0530

    Create day11.c

---
+++ day11.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    
+    int A[100][100], B[100][100], C[100][100];
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d", C[i][j]);
+            if(j < n-1) printf(" ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
