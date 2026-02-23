225a70485689894f8498507cf3c60e71a98e377c
kanikaaaa99999 <kooslovv@gmail.com>
2026-02-23 07:54:44 +0530

    Create day23.c

---
+++ day23.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+    struct Node* dummy = createNode(0);
+    struct Node* tail = dummy;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data <= l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy->next;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m, x;
+    struct Node* list1 = NULL;
+    struct Node* list2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        list1 = insertEnd(list1, x);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &x);
+        list2 = insertEnd(list2, x);
+    }
+
+    struct Node* merged = mergeLists(list1, list2);
+
+    printList(merged);
+
+    return 0;
+}
